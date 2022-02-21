771718d268cf64fb4afba123703d6af8d2261508
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-02-21 11:08:18 -0300

    Update 1.2
    - Reorganização dos arquivos;
    - Criação do documento "teste.js;
    - Criação do documento "comentarios.js".

---
+++ testes-e-praticas/comentarios.js
@@ -0,0 +1,10 @@
+// Aqui é um comentário em linha
+
+/*  Aqui é um comentário em bloco
+    Ninguém verá o que está aqui
+    Somente você!
+*/
+
+console.log('Olá, Mundo!') // Um script falando 'Olá, Mundo!'
+
+// console.log('Não irá ver')
