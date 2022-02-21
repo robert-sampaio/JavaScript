09ca80e399c57cfbf48d5ca4383ead82b1e7b976
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-02-21 20:30:55 -0300

    Update 1.5
    - Criação do documento "var.js";
    - Criação do documento "let-const.js".

---
+++ testes-e-praticas/let-const.js
@@ -0,0 +1,4 @@
+{
+    let x = 0
+    console.log('> Existe um X antes do bloco', x)
+}
---
+++ testes-e-praticas/var.js
@@ -0,0 +1,7 @@
+console.log('> Existe um X antes do bloco', x)
+
+{
+    var x = 0
+}
+
+console.log('> Existe um X antes do bloco', x)
