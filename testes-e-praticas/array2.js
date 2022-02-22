d9f4b4865951920d95f4c7331458219092cdc582
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-02-21 22:20:12 -0300

    Update 1.6
    - Criação do documento "conca-inter.js";
    - Criação do documento "array2.js".

---
+++ testes-e-praticas/array2.js
@@ -0,0 +1,32 @@
+/*
+const animals = [
+    'Lion',
+    'Bat',
+    'Snake'
+]
+*/
+
+//Escolher um dos dados
+//console.log(animals[1])
+
+//Quantidade de dados
+//console.log(animals.length)
+
+const persons = [
+    {
+        name: "Robert",
+        age: 18
+    },
+
+    {
+        name: "Abner",
+        age: 18
+    },
+
+    {
+        name: "Milena",
+        age: 15
+    }
+]
+
+console.log(persons)
---
+++ testes-e-praticas/conca-inter.js
@@ -0,0 +1,12 @@
+var name
+name = 'Robert'
+
+let age, isHuman
+age = 18
+isHuman = 'True'
+
+//Concatenando os valores
+//console.log('O ' + name + ' tem ' + age + ' anos.')
+
+//Interpolando valores com template literals or template strings
+console.log(`O ${name} tem ${age} anos.`)
