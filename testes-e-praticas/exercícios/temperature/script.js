263bd6e2412be866b9466e873b5a067824f63fe0
SlyShieda <98532285+robert-sampaio@users.noreply.github.com>
2022-03-02 12:37:55 -0300

    Update 7.2
    - Creation the document "index.html";
    - Creation the document "script.js"

---
+++ testes-e-praticas/exercícios/temperature/script.js
@@ -0,0 +1,31 @@
+// transformDegree('50F')
+function transformDegree(degree) {
+    const celsiusExists = degree.toUpperCase().includes('C')
+    const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    if(!celsiusExists && !fahrenheitExists) {
+        throw new Error('Grau não identificado')
+    }
+
+    // Fluxo ideal, F -> C
+    let updatedDegree = Number(degree.toUpperCase().replace('F', ""));
+    let formula = fahrenheit => (fahrenheit - 32) * 5/9
+    let degreeSign = 'C'
+
+    // Fluxo alternativo, C -> F
+    if(celsiusExists) {
+    updatedDegree = Number(degree.toUpperCase().replace('C', ""));
+    formula = celsius => celsius * 9/5 + 32
+    degreeSign = 'F'
+    }
+
+    return formula(updatedDegree) + degreeSign
+}
+
+try {
+    console.log(transformDegree('50C'))
+    console.log(transformDegree('50F'))
+    transformDegree('50Z')
+} catch (error) {
+    console.log(error.message)
+}
